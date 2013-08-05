48336ea74d183888d5add17045de1524bd09d97e
phaer <phaer@phaer.org>
2013-08-05 15:04:52 +0200

    add timeout for mpay24 confirmation interface.

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,25 +1,35 @@
 jQuery(document).ready(function() {
-    function poll_confirmation_interface() {
-        console.log(Drupal.settings.mpay24_payment.pid);
-        jQuery.ajax({
-            contentType: 'application/json',
-            url: "/mpay24_payment/confirm/status/"+Drupal.settings.mpay24_payment.pid,
-            success: function(data) {
-                if (data.status === null) {
-                    setTimeout(function() { poll_confirmation_interface(); }, 1000);
-                }
-                else if (data.status === 'success') {
-                    window.location.replace(Drupal.settings.mpay24_payment.success_url);
-                } else {
+    mpay24_payment = {
+        current_time_in_seconds: function () {
+            return Math.floor(new Date().getTime() / 1000);
+        },
+        poll: function () {
+            jQuery.ajax({
+                contentType: 'application/json',
+                url: "/mpay24_payment/confirm/status/"+Drupal.settings.mpay24_payment.pid,
+                success: function(data) {
+                    console.log('status', data.status);
+                    if (data.status === null) {
+                        setTimeout(function() { this.poll(); }, 1000);
+                    }
+                    else if (data.status === 'success') {
+                        window.location.replace(Drupal.settings.mpay24_payment.success_url);
+                    } else {
+                        //window.location.replace("/" + Drupal.settings.payment_mpay24.error_url +
+                        //"/" + Drupal.settings.payment_mpay24.pid);
+                    }
+                },
+                error: function(data) {
                     //window.location.replace("/" + Drupal.settings.payment_mpay24.error_url +
                     //"/" + Drupal.settings.payment_mpay24.pid);
-                }
-            },
-            error: function(data) {
-                //window.location.replace("/" + Drupal.settings.payment_mpay24.error_url +
-                //"/" + Drupal.settings.payment_mpay24.pid);
-            },
-        });
-    }
-    poll_confirmation_interface();
+                },
+            });
+        },
+        init: function() {
+            this.start = this.current_time_in_seconds();
+            this.poll();
+        }
+    };
+    window.mpay24_payment = mpay24_payment;
+    mpay24_payment.init();
 });

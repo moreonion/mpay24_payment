6753ed3412797ce95e05f9fec544e0d91c89abe0
Roman Zimmermann <roman@more-onion.com>
2016-11-29 15:13:11 +0100

    Use payment_controller_data.

--- src/ControllerBase.php
+++ src/ControllerBase.php
@@ -47,67 +47,9 @@ class ControllerBase extends \PaymentMethodController {
   }
 
   /**
-   * Helper for entity_load().
-   */
-  public static function load($entities) {
-    $pmids = array();
-    foreach ($entities as $method) {
-      if ($method->controller instanceof PaymentMethodControllerMPay24) {
-        $pmids[] = $method->pmid;
-      }
-    }
-    if ($pmids) {
-      $query = db_select('mpay24_payment_payment_method_controller', 'controller')
-        ->fields('controller')
-        ->condition('pmid', $pmids);
-      $result = $query->execute();
-      while ($data = $result->fetchAssoc()) {
-        $method = $entities[$data['pmid']];
-        unset($data['pmid']);
-        if ($data['testmode']) {
-          $data['merchantid'] = '9' . substr($data['merchantid'], 1);
-        }
-        $method->controller_data = (array) $data;
-        $method->controller_data += $method->controller->controller_data_defaults;
-      }
-    }
-  }
-
-  /**
-   * Helper for entity_insert().
-   */
-  public function insert($method) {
-    $method->controller_data += $this->controller_data_defaults;
-
-    $query = db_insert('mpay24_payment_payment_method_controller');
-    $values = array_merge($method->controller_data, array('pmid' => $method->pmid));
-    $query->fields($values);
-    $query->execute();
-  }
-
-  /**
-   * Helper for entity_update().
-   */
-  public function update($method) {
-    $query = db_update('mpay24_payment_payment_method_controller');
-    $values = array_merge($method->controller_data, array('pmid' => $method->pmid));
-    $query->fields($values);
-    $query->condition('pmid', $method->pmid);
-    $query->execute();
-  }
-
-  /**
-   * Helper for entity_delete().
-   */
-  public function delete($method) {
-    db_delete('mpay24_payment_payment_method_controller')
-      ->condition('pmid', $method->pmid)
-      ->execute();
-  }
-
-  /**
    * Allows the controller to act when the payment status is polled.
    */
   public function pollStatus(Payment $payment) {
   }
+
 }
